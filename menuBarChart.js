b52da29ee91dc3e8a8fb48a32aefcc7bc199382c
Alvin <alvinchun911@gmail.com>
2019-01-03 23:57:34 -0500

    updated

--- menuBarChart.js
+++ menuBarChart.js
@@ -13,13 +13,19 @@ const graphHeight = 600 - margin.top - margin.bottom;
 const graph = svg
   .append("g")
   .attr("width", graphWidth)
-  .attr("height", graphHeight);
+  .attr("height", graphHeight)
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+const xAxisGroup = graph
+  .append("g")
+  .attr("transform", `translate(0,${graphHeight})`);
+const yAxisGroup = graph.append("g");
 
 d3.json("menu.json").then(data => {
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d.orders)])
-    .range([0, 500]);
+    .range([graphHeight, 0]);
 
   // const min = d3.min(data, d => d.orders);
   // const max = d3.max(data, d => d.orders);
@@ -43,7 +49,7 @@ d3.json("menu.json").then(data => {
   // console.log(y(900));
 
   // join the data to rects
-  const rects = svg.selectAll("rect").data(data);
+  const rects = graph.selectAll("rect").data(data);
 
   //Real DOM with attribute
   // Use bandwidth to scale the width properties we set in x above (paddingInner and paddingOuter)
@@ -60,7 +66,26 @@ d3.json("menu.json").then(data => {
     .enter()
     .append("rect")
     .attr("width", x.bandwidth)
-    .attr("height", d => y(d.orders))
+    .attr("height", d => graphHeight - y(d.orders))
     .attr("fill", "orange")
-    .attr("x", d => x(d.name));
+    .attr("x", d => x(d.name))
+    .attr("y", d => y(d.orders));
+
+  // create and call the axes
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + " orders");
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+
+  //text anchor = default (locating the text middle of the text anchor point)
+  //but we can try to rotate with middle point with end of the text
+  xAxisGroup
+    .selectAll("text")
+    .attr("transform", "rotate(-10)")
+    .attr("text-anchor", "end")
+    .attr("fill", "blue");
 });
